8b48a457c240f070e602a5cfc6bf17a638b7b09b
Tashi Kashi <t.kashi.92@gmail.com>
2018-01-18 20:44:28 +0000

    learned to use sorting for objects

---
+++ advancedSorting.js
@@ -0,0 +1,31 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+/*
+var alphaBet = students.sort(({name}) => {
+  console.log()
+});
+
+console.log(alphaBet);
+*/
+
+console.log(students.sort( (a, b) => {
+
+  var namesA = a.name;
+  var nameB = b.name;
+
+  if (namesA < nameB){
+    return -1;
+  }
+
+  if (namesA > nameB){
+    return 1;
+  }
+  if (namesA = nameB) {
+    return b.age - a.age;
+  };
+}));
